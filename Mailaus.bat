43ff48ae91baa763231f08101c5e844d3710dcca
MangoTheSherlock <manuel.saemann1@outlook.de>
2025-03-25 11:11:47 +0100

    Mailaus.bat aktualisieren

--- Mailaus.bat
+++ Mailaus.bat
@@ -2,6 +2,4 @@
 :loop
     powershell -ExecutionPolicy Bypass -File "%cd%\Mailspam.ps1"
     echo Ausfuehrung %%i von %count% abgeschlossen.
-
-echo Alle %count% Ausfuehrungen des PowerShell-Skripts wurden abgeschlossen!
 goto loop
